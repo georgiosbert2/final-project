a7f81e6cb6b820ec2a9b92b4c182b849ee47966e
georgiosbert2 <georgiosbert2@gmail.com>
2025-04-11 12:37:08 +0000

    Add 34.cpp

---
+++ src/34.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num1 = 5;
+    int num2 = 3;
+    float average = (num1 + num2) / 2.0f;
+    cout << "The average of " << num1 << " and " << num2 << " is: " << average << endl;
+    return 0;
+}
