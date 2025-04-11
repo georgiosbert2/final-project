c3e30a29c90944b68ffacc53fd78837b46313c08
georgiosbert2 <georgiosbert2@gmail.com>
2025-04-11 12:51:09 +0000

    Add 41.cpp

---
+++ src/41.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x = 5;
+    double y = 3.14;
+
+    // Perform some operations on x and y
+
+    cout << "x is " << x << endl;
+    cout << "y is " << y << endl;
+
+    return 0;
+}
