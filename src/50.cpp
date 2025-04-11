baba4bd32406356e968bb72861a5b0d796b43efb
georgiosbert2 <georgiosbert2@gmail.com>
2025-04-11 14:07:12 +0000

    Add 50.cpp

---
+++ src/50.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    int x = 5;
+    if(x > 10) {
+        std::cout << "x is greater than 10" << std::endl;
+    } else {
+        std::cout << "x is not greater than 10" << std::endl;
+    }
+
+    return 0;
+}
