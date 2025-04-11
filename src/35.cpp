3040c9cd0b02dcd789e47227b6bad744da52cf4c
georgiosbert2 <georgiosbert2@gmail.com>
2025-04-11 12:39:24 +0000

    Add 35.cpp

---
+++ src/35.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    int x = 5;
+    int y = 10;
+
+    if (x > y) {
+        std::cout << "x is greater than y" << std::endl;
+    } else {
+        std::cout << "y is greater than x" << std::endl;
+    }
+
+    return 0;
+}
