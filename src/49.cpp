59dde5724e786ad910f9b3c9309ef5f84515b994
georgiosbert2 <georgiosbert2@gmail.com>
2025-04-11 14:05:18 +0000

    Add 49.cpp

---
+++ src/49.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n = 5;
+    int arr[n];
+
+    // Example usage of an array and a loop to print all elements
+    cout << "Array: ";
+    for (int i = 0; i < n; ++i) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
